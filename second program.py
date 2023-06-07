7d5770fd16f6a79009caa3fde72c146393be982b
mirva777 <118059890+mirva777@users.noreply.github.com>
2023-06-07 17:28:55 +0500

    second program.py

---
+++ second program.py
@@ -0,0 +1,25 @@
+import requests
+
+# Set your Pangea API credentials
+access_key = "YOUR_ACCESS_KEY"
+secret_key = "YOUR_SECRET_KEY"
+
+# Create the request headers
+headers = {
+    "X-Pangea-Access-Key": access_key,
+    "X-Pangea-Secret-Key": secret_key,
+}
+
+# Make the request to the Pangea API
+url = "https://api.pangea.cloud/v1/users?organization_id=1234567890"
+response = requests.get(url, headers=headers)
+
+# Check the response status code
+if response.status_code == 200:
+    # The request was successful, print the users
+    users = response.json()["users"]
+    for user in users:
+        print(f"User ID: {user['id']} Name: {user['name']}")
+else:
+    # The request failed, print the error message
+    print(response.json()["error"])
